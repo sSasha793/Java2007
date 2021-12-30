02fca8d093136c6ecbbdf80794a6002817270ab5
Sasha <limonchik7922gmail.com>
2021-12-30 23:09:45 +0200

    Домашка 2

---
+++ src/main/java/lesson3/HomeWorkApp3.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+public class HomeWorkApp3 {
+    public static void main(String[] args) {
+        String str = "new String ";
+        String str2 = new String("new String");
+
+        str.equals(str2);
+        boolean g = str == str2;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
